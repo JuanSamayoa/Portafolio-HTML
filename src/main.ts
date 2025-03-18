3caa32ca6264895ea06fd7a2d2351d9e2d53efa5
JuanSamayoa <juancho1705@mail.com>
2025-03-17 18:22:23 -0600

    Deploy update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 // main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [provideRouter(routes, withHashLocation()), provideAnimations()],
 }).catch((err) => console.error(err));

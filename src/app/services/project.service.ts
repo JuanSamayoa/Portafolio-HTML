a04a889074614d2242e6cd73b8303f9847bcbd52
JuanSamayoa <juancho1705@gmail.com>
2025-03-23 22:29:11 -0600

    deploy: auto deploy update

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 export interface Project {
   id: number;
   title: string;
+  shortDescription: string;
   description: string;
   imageUrl: string;
   technologies: string[];
@@ -19,15 +20,17 @@ export class ProjectService {
     {
       id: 1,
       title: 'Sistemas de Recursos Humanos',
+      shortDescription: 'Sistema de Recursos Humanos con capacidad de reportes y gestión de empleados',
       description:
-        'Sistema de Recursos Humanos para empresa, el cual gestiona desde nuevos empleados hasta las vacaciones de los mismos. Con capacidad de generar reportes.',
-      imageUrl: 'assets/images/project1.jpg',
-      technologies: ['Java', 'SQL', 'Swing'],
+        'El Sistema de Recursos Humanos es una aplicación diseñada para la gestión eficiente del personal dentro de una organización. Su objetivo es organizar y coordinar todas las actividades relacionadas con la contratación, administración de empleados, permisos, licencias, vacaciones y movimientos dentro de la empresa. Además, ofrece un módulo de reportes para la toma de decisiones estratégicas en el departamento de Recursos Humanos.',
+      imageUrl: 'assets/img/humanresources.webp',
+      technologies: ['Java', 'SQL', 'Swing', 'SQL Server'],
       githubUrl: 'https://github.com/JuanSamayoa/SistemaRecursosHumanos',
     },
     {
       id: 2,
       title: 'Project 2',
+      shortDescription: 'Description of project 2',
       description: 'Description of project 2',
       imageUrl: 'assets/images/project2.jpg',
       technologies: ['React', 'JavaScript', 'Material UI'],
@@ -36,6 +39,7 @@ export class ProjectService {
     {
       id: 3,
       title: 'Project 3',
+      shortDescription: 'Description of project 3',
       description: 'Description of project 3',
       imageUrl: 'assets/images/project3.jpg',
       technologies: ['Node.js', 'Express', 'MongoDB'],
@@ -44,6 +48,7 @@ export class ProjectService {
     {
       id: 3,
       title: 'Project 4',
+      shortDescription: 'Description of project 4',
       description: 'Description of project 4',
       imageUrl: 'assets/images/project4.jpg',
       technologies: ['Node.js', 'Express', 'MongoDB'],

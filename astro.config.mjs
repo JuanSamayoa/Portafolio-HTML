3c6ff8d9faff412e1b500c529539614718a72d0b
JuanSamayoa <juancho1705@gmail.com>
2025-04-11 19:03:32 -0600

    deploy: auto deploy update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  markdown: {
-    shikiConfig: {
+  site: 'https://juan-samayoa.is-a.dev',
+  base: '/',
+  build: {
+    assets: 'assets',
+  },
+  vite: {
+    build: {
+      assetsInlineLimit: 0, // Evita la incrustación inline de assets pequeños
     },
   },
 });
